10ebd130d489e1c4660b7d495d2649a0891f20e6
puzzleleaf <cmtyx2@naver.com>
2020-08-27 23:07:59 +0900

    auth_failure , facade

---
+++ 1. ddd/lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:domain_driven/domain/auth/auth_failure.dart';
+import 'package:flutter/foundation.dart';
+import 'value_objects.dart';
+
+/**
+ * https://ko.wikipedia.org/wiki/%ED%8D%BC%EC%82%AC%EB%93%9C_%ED%8C%A8%ED%84%B4
+ */
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
