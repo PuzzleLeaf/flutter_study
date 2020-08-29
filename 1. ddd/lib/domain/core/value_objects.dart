b8ddc12d33095ee940676eb9db510c88b275e605
puzzleleaf <cmtyx2@naver.com>
2020-08-29 14:21:29 +0900

    firebase auth facade

--- 1. ddd/lib/domain/core/value_objects.dart
+++ 1. ddd/lib/domain/core/value_objects.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:domain_driven/domain/core/errors.dart';
 import 'package:domain_driven/domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,6 +8,13 @@ import 'package:flutter/foundation.dart';
 abstract class ValueObjects<T> {
   ValueObjects();
   Either<ValueFailure<T>, T> value;
+
+  /// [ValueFailure]을 포함한 [UnexpectedValueError] 에러를 발생
+  T getOrCrash() {
+    // dartz에 현재 값을 그대로 반환하는 id가 존재
+    // identity = id - some as writing (right) => right
+    return value.fold((l) => throw UnexpectedValueError(l), id);
+  }
 
   bool isValid() => value.isRight();
 
